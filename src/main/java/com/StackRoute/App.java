14da9cb5b30f7648f2548e828fdb620ab2a070c3
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-07-29 22:25:44 +0530

    Task5 completed

--- src/main/java/com/StackRoute/App.java
+++ src/main/java/com/StackRoute/App.java
@@ -3,16 +3,21 @@ package com.StackRoute;
 
 import com.StackRoute.domain.Movie;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigurationClass.class);
+
+        ConfigurableApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigurationClass.class);
+//
         Movie movie = applicationContext.getBean("movie",Movie.class);
         System.out.println(movie);
+        applicationContext.close();
 
     }
 }
--- src/main/java/com/StackRoute/ConfigurationClass.java
+++ src/main/java/com/StackRoute/ConfigurationClass.java
@@ -9,12 +9,15 @@ import org.springframework.stereotype.Component;
 @PropertySource("Actor.properties")
 public class ConfigurationClass {
 
-
-
     @Bean
     public Movie movie(){
         Movie movie=new Movie();
         return movie;
+    }
+    @Bean(name = "lifeCycle")
+    public BeanLifeCycleDemo beanLifeCycleDemo(){
+        BeanLifeCycleDemo beanLifeCycleDemo=new BeanLifeCycleDemo();
+        return beanLifeCycleDemo;
     }
 
 }
